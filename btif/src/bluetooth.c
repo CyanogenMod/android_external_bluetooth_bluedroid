657a71dde29dd35f947775fb4e35a9d77b233dd2
Matthew Xie <mattx@google.com>
2012-06-07 22:45:43 -0700

    make HAL_MODULE_INFO_SYM non const

--- btif/src/bluetooth.c
+++ btif/src/bluetooth.c
@@ -403,7 +403,7 @@ static struct hw_module_methods_t bt_stack_module_methods = {
     .open = open_bluetooth_stack,
 };
 
-const struct hw_module_t HAL_MODULE_INFO_SYM = {
+struct hw_module_t HAL_MODULE_INFO_SYM = {
     .tag = HARDWARE_MODULE_TAG,
     .version_major = 1,
     .version_minor = 0,
