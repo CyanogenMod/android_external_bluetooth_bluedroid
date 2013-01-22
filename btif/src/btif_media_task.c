6f3728a697b9221d553725fd2cbdb639d38efa9f
Sunny Kapdi <sunnyk@codeaurora.org>
2013-01-22 13:21:26 -0800

    Bluetooth: Change Sampling Frequency to 48 kHz
    Change Default Sampling Frequency to 48 kHz by default to
    match Audio Source.
    CRs-fixed: 436686
    
    Change-Id: I592b1afb351455e3222021640a29eefe120c4c9b

--- btif/src/btif_media_task.c
+++ btif/src/btif_media_task.c
@@ -1,6 +1,7 @@
 /******************************************************************************
  *
  *  Copyright (C) 2009-2012 Broadcom Corporation
+ *  Copyright (c) 2013, Linux Foundation. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -763,8 +764,8 @@ void btif_a2dp_setup_codec(void)
 
     GKI_disable();
 
-    /* for now hardcode 44.1 khz 16 bit stereo */
-    media_feeding.cfg.pcm.sampling_freq = 44100;
+    /* for now hardcode 48 khz 16 bit stereo */
+    media_feeding.cfg.pcm.sampling_freq = 48000;
     media_feeding.cfg.pcm.bit_per_sample = 16;
     media_feeding.cfg.pcm.num_channel = 2;
     media_feeding.format = BTIF_AV_CODEC_PCM;
