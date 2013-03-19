e8e9070f3ec242d85b70f77b63dc32d94920a425
ravindranath <ravindranathx.doddi@intel.com>
2013-03-19 11:35:44 -0400

    Support USB HCI
    Issue: AXIA-1459
    Change-Id: Ie4cc5766446774a0bae3bbf7d9baa5f44e814f59
    Signed-off-by: Ravindranath Doddi <ravindranathx.doddi@intel.com>
    Signed-off-by: Krishnan V <krishnanx.vaidyanathan.venkitakrishnan@intel.com>
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

--- hci/Android.mk
+++ hci/Android.mk
@@ -25,11 +25,22 @@ LOCAL_SRC_FILES += \
 
 endif
 
+ifeq ($(BLUETOOTH_HCI_USE_USB),true)
+LOCAL_SRC_FILES += \
+        src/usb.c
+
+LOCAL_C_INCLUDES += \
+        external/libusb
+
+LOCAL_SHARED_LIBRARIES := \
+        libusb
+endif
+
 LOCAL_C_INCLUDES += \
         $(LOCAL_PATH)/include \
         $(LOCAL_PATH)/../utils/include
 
-LOCAL_SHARED_LIBRARIES := \
+LOCAL_SHARED_LIBRARIES += \
         libcutils \
         liblog \
         libdl \
