8d12535d50d82bd0e30c9a385051f41cf8fddb20
Steve Kondik <shade@chemlab.org>
2013-08-10 02:09:58 -0700

    bluedroid: Remove ALSA check on sample rate setting
     * All QC devices using the CodeAurora drivers should be using a 48KHz
       samplerate for A2DP.
    
    Change-Id: I8cec354c5a2dde0f85f19cfee2f75501f2a2d5a3

--- audio_a2dp_hw/Android.mk
+++ audio_a2dp_hw/Android.mk
@@ -2,10 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifeq ($(BOARD_USES_ALSA_AUDIO),true)
 ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf)
 	LOCAL_CFLAGS += -DSAMPLE_RATE_48K
-endif
 endif
 
 LOCAL_SRC_FILES:= \
