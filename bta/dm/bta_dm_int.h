dac7a11f08c91dd398341763f23b614b4b827107
Hemant Gupta <hemantg@codeaurora.org>
2013-08-12 20:06:15 +0530

    Bluetooth: Initiate RNR for HID Pointing Devices if no name.
    This patch initiates Remote Name Request for HID Pointing devices in case
    name is not known before initiating HID L2CAP Connection. Once remote name
    is retrieved, name is stored in NVRAM and upper layers is informed
    of name of remote device.
    
    Change-Id: I6ecf07c222f804dcfcedd84fc1ff26ffe5bcbdc8
    CRs-Fixed: 519771
    (cherry picked from commit c9275e5e411009fae1369cb2e9d6d871d8af47e0)
    (cherry picked from commit fcc4a0f5d64858278c3562ae1dc8a034c36fedc1)
    (cherry picked from commit 601101f5526acec343636ad3a3ff69f1210c9ee2)

--- bta/dm/bta_dm_int.h
+++ bta/dm/bta_dm_int.h
@@ -89,6 +89,7 @@ enum
 #endif /* BTM_OOB_INCLUDED */
 
     BTA_DM_API_REMOVE_DEVICE_EVT,
+    BTA_DM_API_REM_NAME_EVT,
 
 #if BLE_INCLUDED == TRUE
     BTA_DM_API_ADD_BLEKEY_EVT,
@@ -286,6 +287,14 @@ typedef struct
     BT_HDR      hdr;
     BD_ADDR     bd_addr;
 } tBTA_DM_API_PASKY_CANCEL;
+
+/* data type for BTA_DM_API_REM_NAME_EVT*/
+typedef struct
+{
+    BT_HDR      hdr;
+    tBTA_DM_REM_NAME_CBACK * p_cback;
+    BD_ADDR     bd_addr;
+} tBTA_DM_API_REM_NAME;
 
 /* data type for BTA_DM_CI_IO_REQ_EVT */
 typedef struct
@@ -590,6 +599,8 @@ typedef union
 
     tBTA_DM_API_BOND_CANCEL bond_cancel;
 
+    tBTA_DM_API_REM_NAME remote_name;
+
     tBTA_DM_API_PIN_REPLY pin_reply;
     tBTA_DM_API_LINK_POLICY link_policy;
 
@@ -776,6 +787,7 @@ typedef struct
 #endif
 
     tBTA_DM_ENCRYPT_CBACK      *p_encrypt_cback;
+    tBTA_DM_REM_NAME_CBACK     *p_rem_name_cback;
     tBTA_DM_BLE_SEC_ACT         sec_act;
     TIMER_LIST_ENT              switch_delay_timer;
 
@@ -977,6 +989,7 @@ extern void bta_dm_tx_inqpower(tBTA_DM_MSG *p_data);
 extern void bta_dm_acl_change(tBTA_DM_MSG *p_data);
 extern void bta_dm_add_device (tBTA_DM_MSG *p_data);
 extern void bta_dm_remove_device (tBTA_DM_MSG *p_data);
+extern void bta_dm_remote_name(tBTA_DM_MSG *p_data);
 extern void bta_dm_close_acl(tBTA_DM_MSG *p_data);
 
 
