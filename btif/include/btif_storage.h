codeaurora-unoffical/platform-external-bluetooth-bluedroid

commit b6264c2464da28359291eb6785479b236bfc11e7

Bluetooth: Store bonded HID device info into NVRAM.
Store ssr_max_latency and ssr_min_tout for bonded
HID device into NVRAM.

Change-Id: If24521ec20ba373ab8c74c72f93fd4f6770eb87c

CRs-fixed: 476133

btif/include/btif_storage.h
@@ -235,7 +235,8 @@ bt_status_t btif_storage_add_hid_device_info(bt_bdaddr_t *remote_bd_addr,
                                                     UINT16 attr_mask, UINT8 sub_class,
                                                     UINT8 app_id, UINT16 vendor_id,
                                                     UINT16 product_id, UINT16 version,
-                                                    UINT8 ctry_code, UINT16 dl_len,
+                                                    UINT8 ctry_code, UINT8 ssr_max_lat,
+                                                    UINT8 ssr_min_tout, UINT16 dl_len,
                                                     UINT8 *dsc_list);
 
 /*******************************************************************************

btif/src/btif_hh.c
@@ -901,6 +901,7 @@ static void btif_hh_upstreams_evt(UINT16 event, char* p_param)
                     ret = btif_storage_add_hid_device_info(&(p_dev->bd_addr), p_dev->attr_mask,p_dev->sub_class,p_dev->app_id,
                                                         p_data->dscp_info.vendor_id, p_data->dscp_info.product_id,
                                                         p_data->dscp_info.version,   p_data->dscp_info.ctry_code,
+                                                        p_data->dscp_info.ssr_max_latency,p_data->dscp_info.ssr_min_tout,
                                                         len, p_data->dscp_info.descriptor.dsc_list);
 
                     ASSERTC(ret == BT_STATUS_SUCCESS, "storing hid info failed", ret);

btif/src/btif_storage.c
@@ -861,7 +861,8 @@ bt_status_t btif_storage_add_hid_device_info(bt_bdaddr_t *remote_bd_addr,
                                                     UINT16 attr_mask, UINT8 sub_class,
                                                     UINT8 app_id, UINT16 vendor_id,
                                                     UINT16 product_id, UINT16 version,
-                                                    UINT8 ctry_code, UINT16 dl_len, UINT8 *dsc_list)
+                                                    UINT8 ctry_code, UINT8 ssr_max_lat,
+                                                    UINT8 ssr_min_tout, UINT16 dl_len, UINT8 *dsc_list)
 {
     bdstr_t bdstr;
     bd2str(remote_bd_addr, &bdstr);
@@ -872,6 +873,8 @@ bt_status_t btif_storage_add_hid_device_info(bt_bdaddr_t *remote_bd_addr,
     btif_config_set_int("Remote", bdstr, "HidProductId", product_id);
     btif_config_set_int("Remote", bdstr, "HidVersion", version);
     btif_config_set_int("Remote", bdstr, "HidCountryCode", ctry_code);
+    btif_config_set_int("Remote", bdstr, "ssrmaxlat", ssr_max_lat);
+    btif_config_set_int("Remote", bdstr, "ssrmintout", ssr_min_tout);
     if(dl_len > 0)
         btif_config_set("Remote", bdstr, "HidDescriptor", (const char*)dsc_list, dl_len, BTIF_CFG_TYPE_BIN);
     return BT_STATUS_SUCCESS;
@@ -929,6 +932,12 @@ bt_status_t btif_storage_load_bonded_hid_info(void)
 
             btif_config_get_int("Remote", kname, "HidCountryCode", &value);
             dscp_info.ctry_code = (uint8_t) value;
+
+            btif_config_get_int("Remote", kname, "ssrmaxlat", &value);
+            dscp_info.ssr_max_latency = (uint8_t) value;
+
+            btif_config_get_int("Remote", kname, "ssrmintout", &value);
+            dscp_info.ssr_min_tout = (uint8_t) value;
 
             int len = 0;
             int type;

CyanogenMod/android_external_bluetooth_bluedroid

commit f80d739f6b7ece608cf24bc050b4fb1c4e997a48

Bluetooth: Fix type of max latency and min timeout used in NVRAM
This patch fixes regression caused by commit
b6264c2464da28359291eb6785479b236bfc11e7 where Max latency and Min timeout
were being stored as 8 bit unsigned integers, instead of 16 bits unsigned
integers, resulting in loss of data. Also the values were not being copied
properly resulting in wrong values being sent during BTA_HH_ADD_DEV_EVT.

Change-Id: Ice29944baf29a7ed05698c9f87c3907f2f962a64
CRs-Fixed: 533988

btif/include/btif_storage.h
@@ -236,8 +236,8 @@ bt_status_t btif_storage_add_hid_device_info(bt_bdaddr_t *remote_bd_addr,
                                                     UINT16 attr_mask, UINT8 sub_class,
                                                     UINT8 app_id, UINT16 vendor_id,
                                                     UINT16 product_id, UINT16 version,
-                                                    UINT8 ctry_code, UINT8 ssr_max_lat,
-                                                    UINT8 ssr_min_tout, UINT16 dl_len,
+                                                    UINT8 ctry_code, UINT16 ssr_max_lat,
+                                                    UINT16 ssr_min_tout, UINT16 dl_len,
                                                     UINT8 *dsc_list);
 
 /*******************************************************************************

btif/src/btif_hh.c
@@ -573,6 +573,8 @@ BOOLEAN btif_hh_copy_hid_info(tBTA_HH_DEV_DSCP_INFO* dest , tBTA_HH_DEV_DSCP_INF
     dest->product_id = src->product_id;
     dest->version    = src->version;
     dest->ctry_code  = src->ctry_code;
+    dest->ssr_max_latency  = src->ssr_max_latency;
+    dest->ssr_min_tout  = src->ssr_min_tout;
     return TRUE;
 }
 

btif/src/btif_storage.c
@@ -1362,8 +1362,8 @@ bt_status_t btif_storage_add_hid_device_info(bt_bdaddr_t *remote_bd_addr,
                                                     UINT16 attr_mask, UINT8 sub_class,
                                                     UINT8 app_id, UINT16 vendor_id,
                                                     UINT16 product_id, UINT16 version,
-                                                    UINT8 ctry_code, UINT8 ssr_max_lat,
-                                                    UINT8 ssr_min_tout, UINT16 dl_len, UINT8 *dsc_list)
+                                                    UINT8 ctry_code, UINT16 ssr_max_lat,
+                                                    UINT16 ssr_min_tout, UINT16 dl_len, UINT8 *dsc_list)
 {
     bdstr_t bdstr;
     bd2str(remote_bd_addr, &bdstr);
@@ -1435,10 +1435,10 @@ bt_status_t btif_storage_load_bonded_hid_info(void)
             dscp_info.ctry_code = (uint8_t) value;
 
             btif_config_get_int("Remote", kname, "ssrmaxlat", &value);
-            dscp_info.ssr_max_latency = (uint8_t) value;
+            dscp_info.ssr_max_latency = (uint16_t) value;
 
             btif_config_get_int("Remote", kname, "ssrmintout", &value);
-            dscp_info.ssr_min_tout = (uint8_t) value;
+            dscp_info.ssr_min_tout = (uint16_t) value;
 
             btif_config_get_int("Remote", kname, "ssrmaxlat", &value);
             dscp_info.ssr_max_latency = (uint8_t) value;