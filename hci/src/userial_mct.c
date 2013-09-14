690401a234619e7a836859f6e4458aabdfb7c9e0
Zhihai Xu <zhihaixu@google.com>
2013-09-13 19:31:18 -0700

    a2dp media packet sent too slow, which cause packet overflow.
    the error message: btif_media_aa_prep_2_send congestion buf count 24
    The reason is we increase all bluedroid thread related TX data path priority:
    media, timer, BTU and HCI_WORKER, but we didn't increase the RX data path
    priority: userial_read_thread. If the RX thread is blocked, it will also
    affect TX, because the HCI have flow control, we need receive HCI Number of
     Completed Packets Events to continue send packet to the controller.
    If we HCI complete Packets Event packets is blocked, the HCI flow control
    will block the HCI packet trnasmission until we get HCI complete packets
    event to know how many buffer is available in the controller.
    I tested with a chrysler CIP/VP4 carkit which show this error message,
    After I apply this fix, I can't reproduce this problem any more.
    This look like good fix for this issue.
    
    bug:8252054
    Change-Id: Ic78b9abb69648c50aa63e2b3c8b90038018fb6b5

--- hci/src/userial_mct.c
+++ hci/src/userial_mct.c
@@ -36,6 +36,7 @@
 #include "userial.h"
 #include "utils.h"
 #include "bt_vendor_lib.h"
+#include "bt_utils.h"
 
 /******************************************************************************
 **  Constants & Macros
@@ -147,6 +148,8 @@ static void *userial_read_thread(void *arg)
 
     rx_flow_on = TRUE;
     userial_running = 1;
+
+    raise_priority_a2dp(TASK_HIGH_USERIAL_READ);
 
     while (userial_running)
     {
