6f690be9f1175cb6ce99c79bc51567746a029022
Michael Brehm <djp952@gmail.com>
2013-09-26 17:40:42 -0500

    a2dp: Set the default A2DP bitrate to high quality
    Allows bluedroid to calculate the proper A2DP bitpool size.
    This resolves the main A2DP audio quality issues with Android 4.2,
    but does not address the difference in audio output gain/volume as
    compared with Android 4.1.
    
    Setting the output volume level to approximately 80% in conjunction
    with this change seems to result in comparable audio quality
    
    Change-Id: I7a7c9af4e10545a47e15b4002d6554acab46e6c4

--- btif/src/btif_media_task.c
+++ btif/src/btif_media_task.c
@@ -153,8 +153,8 @@ enum {
 #define BTIF_MEDIA_BITRATE_STEP 5
 #endif
 
-/* Middle quality quality setting @ 48 khz */
-#define DEFAULT_SBC_BITRATE 237
+/* High quality quality setting @ 48 khz */
+#define DEFAULT_SBC_BITRATE 328
 #define SBC_HIGH_QUALITY_BITRATE 345
 
 #ifndef A2DP_MEDIA_TASK_STACK_SIZE
