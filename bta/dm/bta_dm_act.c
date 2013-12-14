8749c4f0cdeeec460177635f217dca5eefc12da5
Srinu Jella <sjella@codeaurora.org>
2013-12-14 20:04:41 +0530

    Bluetooth: GAP: Cancel inquiry as part of Pairing process
    It allows cancellation of inquiry only when the inquiry is active.
    
    Also it cancels the inquiry before going for pairing, this will
    make sure the BTA state machine is in proper state (SEARCH_IDLE)
    to receive the SDP request.
    
    CRs-Fixed: 568906
    Change-Id: I389ed8b10a37f900ab8b09768948ebbbc878d38b

--- bta/dm/bta_dm_act.c
+++ bta/dm/bta_dm_act.c
@@ -1300,7 +1300,7 @@ void bta_dm_search_cancel (tBTA_DM_MSG *p_data)
 
     tBTA_DM_MSG * p_msg;
 
-    if(BTM_IsInquiryActive())
+    if(BTM_IsInquiryActive() || (bta_dm_search_cb.state == BTA_DM_SEARCH_ACTIVE))
     {
         BTM_CancelInquiry();
         bta_dm_search_cancel_notify(NULL);
@@ -2186,6 +2186,12 @@ void bta_dm_search_cancel_transac_cmpl(tBTA_DM_MSG *p_data)
     }
 
     bta_dm_search_cancel_notify(NULL);
+
+    if(bta_dm_search_cb.p_search_queue)
+    {
+        bta_sys_sendmsg(bta_dm_search_cb.p_search_queue);
+        bta_dm_search_cb.p_search_queue = NULL;
+    }
 }
 
 
