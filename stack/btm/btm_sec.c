2a206b811270b17648956195928217a05ce195c0
Juffin Alex Varghese <jalex@codeaurora.org>
2014-03-17 15:07:01 +0530

    Bluetooth:GAP: Handled collision scenerio with headset
    Handled collision scenerio by rejecting remote link key req
    when collision occured in the headset reconnection
    
    Change-Id: Ic6383abfc46ecd251ae483af85d8af3031c91cad
    CRs-Fixed: 627025

--- stack/btm/btm_sec.c
+++ stack/btm/btm_sec.c
@@ -4667,6 +4667,14 @@ void btm_sec_link_key_request (UINT8 *p_bda)
         return;
     }
 
+    if( (btm_cb.pairing_state == BTM_PAIR_STATE_WAIT_PIN_REQ) && (btm_cb.collision_start_time != 0)
+        && (memcmp (btm_cb.p_collided_dev_rec->bd_addr, p_bda, BD_ADDR_LEN) == 0) )
+    {
+        BTM_TRACE_EVENT2 ("btm_sec_link_key_request() rejecting link key req  State: %d   START_TIMEOUT : %d",
+                         btm_cb.pairing_state, btm_cb.collision_start_time);
+        btsnd_hcic_link_key_neg_reply (p_bda);
+        return;
+    }
     if (p_dev_rec->sec_flags & BTM_SEC_LINK_KEY_KNOWN)
     {
         btsnd_hcic_link_key_req_reply (p_bda, p_dev_rec->link_key);
