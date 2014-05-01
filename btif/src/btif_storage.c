28480544514a0b8c915dd95edceccf508156f198
venkata Jagadeesh <vjagad@codeaurora.org>
2014-05-01 19:35:52 +0530

    Bluetooth:GAP: Update Local device name immediately to NV
    Local device name not updating to NV immediately and dut is not
    visible to remote for first time scan after local name change.
    
    Change-Id: Ic47b17ef366a83da9b374077742c00661a2c3014
    CRs-Fixed: 649529

--- btif/src/btif_storage.c
+++ btif/src/btif_storage.c
@@ -281,7 +281,7 @@ static int prop2cfg(bt_bdaddr_t *remote_bd_addr, bt_property_t *prop)
             else btif_config_set_str("Local", "Adapter",
                                 BTIF_STORAGE_KEY_ADAPTER_NAME, value);
             /* save name immediately */
-            btif_config_save();
+            btif_config_flush();
             break;
         case BT_PROPERTY_REMOTE_FRIENDLY_NAME:
             strncpy(value, (char*)prop->val, prop->len);
