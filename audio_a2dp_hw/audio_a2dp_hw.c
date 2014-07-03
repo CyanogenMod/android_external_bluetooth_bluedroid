171c23f2c009c8dceb5ca69729dff7913e23d3e6
Eric Laurent <elaurent@google.com>
2014-07-03 16:09:53 -0700

    audio: deprecate audio_stream_frame_size()
    Bug: 15000850.
    Change-Id: I1edcc78561cdc328ae0d91a1d07b0836ca5e1715

--- audio_a2dp_hw/audio_a2dp_hw.c
+++ audio_a2dp_hw/audio_a2dp_hw.c
@@ -763,7 +763,7 @@ static uint32_t out_get_latency(const struct audio_stream_out *stream)
     FNLOG();
 
     latency_us = ((out->common.buffer_sz * 1000 ) /
-                    audio_stream_frame_size(&out->stream.common) /
+                    audio_stream_out_frame_size(&out->stream) /
                     out->common.cfg.rate) * 1000;
 
 
