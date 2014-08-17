7c39dfe3974f8de55d9a3e4410659547ed133c08
Andre Eisenbach <eisenbach@google.com>
2014-08-17 16:32:26 -0700

    Fix userial build error for Razor/Mako/Flo
    Change-Id: Ia4ecf38de8a017f5624ab7b32f8ac398de15dee4

--- hci/src/userial_mct.c
+++ hci/src/userial_mct.c
@@ -332,6 +332,19 @@ uint16_t userial_write(uint16_t msg_id, const uint8_t *p_data, uint16_t len)
     return ((uint16_t)total);
 }
 
+void userial_close_reader(void) {
+    // Join the reader thread if it is still running.
+    if (userial_running) {
+        send_wakeup_signal(USERIAL_RX_EXIT);
+        int result = pthread_join(userial_cb.read_thread, NULL);
+        USERIALDBG("%s Joined userial reader thread: %d", __func__, result);
+        if (result)
+            ALOGE("%s failed to join reader thread: %d", __func__, result);
+        return;
+    }
+    ALOGW("%s Already closed userial reader thread", __func__);
+}
+
 /*******************************************************************************
 **
 ** Function        userial_close
