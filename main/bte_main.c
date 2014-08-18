a9e42fb8b47b2dbba12470d449f3b0b9dab1139e
Ajay Kumar <ajayku@codeaurora.org>
2014-08-18 12:49:01 +0530

    Bluetooth: Retry mechanism on preload timeout
    Preload process retry attempts increased to 2
    
    CRs-Fixed: 613616
    Change-Id: I6906ec25b4756ec0cf66e94e2afd8a712d00be3d

--- main/bte_main.c
+++ main/bte_main.c
@@ -63,7 +63,7 @@
 
 /* Stack preload process maximum retry attempts  */
 #ifndef PRELOAD_MAX_RETRY_ATTEMPTS
-#define PRELOAD_MAX_RETRY_ATTEMPTS 0
+#define PRELOAD_MAX_RETRY_ATTEMPTS 2
 #endif
 
 /*******************************************************************************
