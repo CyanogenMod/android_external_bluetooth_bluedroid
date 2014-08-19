0bf268032aa684083ef554049aeb15803554e85f
Scott Mertz <smertz@cyngn.com>
2014-08-19 15:54:37 -0700

    avrcp: fix valid notification range
    Change-Id: I1f7ad211d8290a1120985385cdca156032e71cfa

--- stack/include/avrc_defs.h
+++ stack/include/avrc_defs.h
@@ -840,7 +840,7 @@ typedef union
 #define AVRC_IS_VALID_CAP_ID(a)           (((a == AVRC_CAP_COMPANY_ID) || (a == AVRC_CAP_EVENTS_SUPPORTED)) ? TRUE : FALSE)
 
 #define AVRC_IS_VALID_EVENT_ID(a)           (((a >= AVRC_EVT_PLAY_STATUS_CHANGE) && \
-                                              (a <= AVRC_EVT_ADDR_PLAYER_CHANGE)) ? TRUE : FALSE)
+                                              (a <= AVRC_NUM_NOTIF_EVENTS)) ? TRUE : FALSE)
 
 #define AVRC_IS_VALID_ATTRIBUTE(a)          (((((a > 0) && a <= AVRC_PLAYER_SETTING_SCAN)) || \
                                                ((a >= AVRC_PLAYER_SETTING_LOW_MENU_EXT) && \
