af9cbe956ceef3acf2817447e88da3d1b1feb8c1
Rohit Singh <singhr@codeaurora.org>
2014-09-02 15:24:20 +0530

    Bluetooth: Add OS callback function during L2CAP init
    L2CAP tool crashes during BT turn ON as GKI task calls
    OS callback function hence During initalisation of
    l2cap tool ,wake lock and alarm callback function added.
    
    Change-Id: I6dbdc6fb9ef10c5e157715f95c8796e82f800bbc

--- test/l2test_ertm/l2test_ertm.c
+++ test/l2test_ertm/l2test_ertm.c
@@ -582,10 +582,53 @@ static bt_callbacks_t bt_callbacks = {
     NULL  /*le_lpp_rssi_threshold_evt_cb*/
 };
 
+static bool set_wake_alarm(uint64_t delay_millis, bool should_wake, alarm_cb cb, void *data) {
+  static timer_t timer;
+  static bool timer_created;
+
+  if (!timer_created) {
+    struct sigevent sigevent;
+    memset(&sigevent, 0, sizeof(sigevent));
+    sigevent.sigev_notify = SIGEV_THREAD;
+    sigevent.sigev_notify_function = (void (*)(union sigval))cb;
+    sigevent.sigev_value.sival_ptr = data;
+    timer_create(CLOCK_MONOTONIC, &sigevent, &timer);
+    timer_created = true;
+  }
+
+  struct itimerspec new_value;
+  new_value.it_value.tv_sec = delay_millis / 1000;
+  new_value.it_value.tv_nsec = (delay_millis % 1000) * 1000 * 1000;
+  new_value.it_interval.tv_sec = 0;
+  new_value.it_interval.tv_nsec = 0;
+  timer_settime(timer, 0, &new_value, NULL);
+
+  return true;
+}
+
+static int acquire_wake_lock(const char *lock_name) {
+  return BT_STATUS_SUCCESS;
+}
+
+static int release_wake_lock(const char *lock_name) {
+  return BT_STATUS_SUCCESS;
+}
+
+static bt_os_callouts_t callouts = {
+    sizeof(bt_os_callouts_t),
+    set_wake_alarm,
+    acquire_wake_lock,
+    release_wake_lock,
+};
+
+
 void bdt_init(void)
 {
     printf("INIT BT \n");
     status = sBtInterface->init(&bt_callbacks);
+    if (status == BT_STATUS_SUCCESS) {
+        status = sBtInterface->set_os_callouts(&callouts);
+    }
     check_return_status(status);
 }
 
