6d595bc88e26cd5de632e66bf6d538072ec2e21f
Nitin Arora <niarora@codeaurora.org>
2014-10-10 15:45:43 -0700

    Bluetooth: Set iocap for LE connections to be "Keyboard display"
    This changes the IO cap of the LE device to keyboard display
    from "Display yes/no"
    
    Change-Id: Idd0df098f3e283a12d1831b58cad55feeea9ec41

--- btif/co/bta_dm_co.c
+++ btif/co/bta_dm_co.c
@@ -37,7 +37,7 @@ tBTE_APPL_CFG bte_appl_cfg = { 0x1, 0x4, 0x7, 0x7, 0x10 };
 tBTE_APPL_CFG bte_appl_cfg =
 {
     BTM_AUTH_SPGB_YES,  // Authentication requirements
-    BTM_LOCAL_IO_CAPS,  // I/O capabilities
+    BTM_IO_CAP_KBDISP,  // I/O capabilities
     7,                  // Initiaor key size
     7,                  // Responder key size
     16                  // Maximum key size
