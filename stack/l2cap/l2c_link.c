codeaurora-unoffical/platform-external-bluetooth-bluedroid

Bluetooth: Fix for Auto-Reconnect failure after SSR with BT HS
Fix for auto reconnection to BT headset fails after SSR. Issue is
due to connection collision initiated by DUT and remote. There will
be on l2cap controller block allocated between DUT and remote BD
addr. DUT initiated connection fails and releases lcb, when
incoming connection request is ongoing resulting in l2cap hold
timeout and connection fails. Do not released the lcb if there is
a connection collision.

Change-Id: I753080f20c03a6ea3c8412bf9b34d97cf5fc97e6
CRs-fixed: 654968

stack/l2cap/l2c_link.c
@@ -121,6 +121,7 @@ BOOLEAN l2c_link_hci_conn_req (BD_ADDR bd_addr)
         else
             p_lcb->link_role = l2cu_get_conn_role(bd_addr);
 
+        p_lcb->is_collision = TRUE;
         btsnd_hcic_accept_conn (bd_addr, p_lcb->link_role);
 
         p_lcb->link_state = LST_CONNECTING;
@@ -218,7 +219,7 @@ BOOLEAN l2c_link_hci_conn_comp (UINT8 status, UINT16 handle, BD_ADDR p_bda)
         l2c_process_held_packets(FALSE);
 
         btu_stop_timer (&p_lcb->timer_entry);
-
+        p_lcb->is_collision = FALSE;
         /* For all channels, send the event through their FSMs */
         for (p_ccb = p_lcb->ccb_queue.p_first_ccb; p_ccb; p_ccb = p_ccb->p_next_ccb)
         {
@@ -260,6 +261,16 @@ BOOLEAN l2c_link_hci_conn_comp (UINT8 status, UINT16 handle, BD_ADDR p_bda)
 
         p_lcb->disc_reason = status;
         /* Release the LCB */
+        if(status == HCI_ERR_COMMAND_DISALLOWED && p_lcb->is_collision ==  TRUE)
+        {
+          L2CAP_TRACE_ERROR0 ("l2c_link_hci_conn_comp: collision,Dont free p_lcb");
+          p_lcb->is_collision = FALSE;
+          p_lcb->link_state = LST_CONNECTING;
+          return (TRUE);
+        }
+
+        p_lcb->is_collision = FALSE;
+
         if (p_lcb->ccb_queue.p_first_ccb == NULL)
             l2cu_release_lcb (p_lcb);
         else                              /* there are any CCBs remaining */

CyanogenMod/android_external_bluetooth_bluedroid

stack/l2cap/l2c_link.c
@@ -159,6 +159,7 @@ BOOLEAN l2c_link_hci_conn_req (BD_ADDR bd_addr)
         else
             p_lcb->link_role = l2cu_get_conn_role(p_lcb);
 
+        p_lcb->is_collision = TRUE;
         btsnd_hcic_accept_conn (bd_addr, p_lcb->link_role);
 
         p_lcb->link_state = LST_CONNECTING;
@@ -256,7 +257,7 @@ BOOLEAN l2c_link_hci_conn_comp (UINT8 status, UINT16 handle, BD_ADDR p_bda)
         l2c_process_held_packets(FALSE);
 
         btu_stop_timer (&p_lcb->timer_entry);
-
+        p_lcb->is_collision = FALSE;
         /* For all channels, send the event through their FSMs */
         for (p_ccb = p_lcb->ccb_queue.p_first_ccb; p_ccb; p_ccb = p_ccb->p_next_ccb)
         {
@@ -298,6 +299,16 @@ BOOLEAN l2c_link_hci_conn_comp (UINT8 status, UINT16 handle, BD_ADDR p_bda)
 
         p_lcb->disc_reason = status;
         /* Release the LCB */
+        if(status == HCI_ERR_COMMAND_DISALLOWED && p_lcb->is_collision ==  TRUE)
+        {
+          L2CAP_TRACE_ERROR ("l2c_link_hci_conn_comp: collision,Dont free p_lcb");
+          p_lcb->is_collision = FALSE;
+          p_lcb->link_state = LST_CONNECTING;
+          return (TRUE);
+        }
+
+        p_lcb->is_collision = FALSE;
+
         if (p_lcb->ccb_queue.p_first_ccb == NULL)
             l2cu_release_lcb (p_lcb);
         else                              /* there are any CCBs remaining */