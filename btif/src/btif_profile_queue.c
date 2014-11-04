codeaurora-unoffical/platform-external-bluetooth-bluedroid

Bluetooth: Delete Connect Req from Queue.
Delete connect request node from connect queue
when app sends connect request for same UUID.

Change-Id: I3dec215f09050ef2290992288c2fe9f7672af102
CRs-Fixed: 614558

btif/src/btif_profile_queue.c
@@ -101,6 +101,40 @@ static bt_status_t queue_int_connect_next()
     return (*(btif_connect_cb_t*)p_head->p_cb)(&p_head->bda);
 }
 
+static void queue_check_connect(connect_node_t *p_param)
+{
+    BTIF_TRACE_VERBOSE2("%s, UUID : 0x%x", __FUNCTION__, p_param->uuid);
+    connect_node_t *p_head = connect_queue;
+    connect_node_t *p_temp = p_head;
+    if (connect_queue == NULL)
+        return;
+    // need to delete whole list in case of single node.
+    if (p_head != NULL && connect_queue->p_next == NULL)
+    {
+        if ((p_param->uuid == p_head->uuid))
+        {
+            BTIF_TRACE_VERBOSE0("Matched Connect req with uuid for single node");
+            connect_queue = connect_queue->p_next;
+            GKI_freebuf(p_head);
+            return;
+        }
+    }
+    // parse list for common node and delete that node
+    while (p_head != NULL && connect_queue->p_next != NULL)
+    {
+        if ((p_param->uuid == p_head->uuid))
+        {
+            BTIF_TRACE_VERBOSE0("Matched Connect req with uuid");
+            // delete this node
+            p_temp->p_next = p_head->p_next;
+            GKI_freebuf(p_head);
+            break;
+        }
+        p_temp = p_head;
+        p_head = p_head->p_next;
+    }
+}
+
 static void queue_int_handle_evt(UINT16 event, char *p_param)
 {
 
@@ -117,6 +151,9 @@ static void queue_int_handle_evt(UINT16 event, char *p_param)
         case BTIF_QUEUE_PENDING_CONECT_EVT:
             queue_int_add((connect_node_t*)p_param);
             return;
+            break;
+        case BTIF_QUEUE_CHECK_CONNECT_REQ:
+            queue_check_connect((connect_node_t*)p_param);
             break;
         default:
             BTIF_TRACE_VERBOSE0("BTIF_QUEUE_PENDING_CONECT_ADVANCE_EVT");
@@ -201,5 +238,24 @@ void btif_queue_release()
     }
 
     connect_queue = NULL;
+}
+/*******************************************************************************
+**
+** Function         btif_queue_remove_connect
+**
+** Description      Remove connection request from connect Queue
+**                  when connect request for same UUID is received
+**                  from app.
+**
+** Returns          void
+**
+*******************************************************************************/
+void btif_queue_remove_connect(uint16_t uuid, uint8_t check_connect_req)
+{
+    connect_node_t node;
+    memset(&node, 0, sizeof(connect_node_t));
+    node.uuid = uuid;
+    btif_transfer_context(queue_int_handle_evt, check_connect_req,
+                      (char*)&node, sizeof(connect_node_t), NULL);
 }
 

CyanogenMod/android_external_bluetooth_bluedroid

btif/src/btif_profile_queue.c
@@ -36,11 +36,6 @@
 **  Local type definitions
 *******************************************************************************/
 
-typedef enum {
-    BTIF_QUEUE_CONNECT_EVT,
-    BTIF_QUEUE_ADVANCE_EVT,
-}btif_queue_event_t;
-
 typedef struct {
     bt_bdaddr_t bda;
     uint16_t uuid;
@@ -92,6 +87,29 @@ static bt_status_t queue_int_connect_next() {
     return p_head->connect_cb(&p_head->bda, p_head->uuid);
 }
 
+static bool queue_check_uuid(void *data, void *cb_data)
+{
+    connect_node_t *node = (connect_node_t *)data;
+    uint16_t uuid = *(uint16_t*)cb_data;
+
+    BTIF_TRACE_VERBOSE("%s, UUID : 0x%x", __FUNCTION__, uuid);
+    if(node->uuid == uuid)
+    {
+        list_remove(connect_queue, data);
+    }
+    return true;
+}
+
+static void queue_check_connect(connect_node_t *p_param)
+{
+    BTIF_TRACE_VERBOSE("%s, UUID : 0x%x", __FUNCTION__, p_param->uuid);
+
+    if (connect_queue == NULL)
+        return;
+
+    list_foreach_ext(connect_queue, queue_check_uuid, (void*)&(p_param->uuid));
+}
+
 static void queue_int_handle_evt(UINT16 event, char *p_param)
 {
     BTIF_TRACE_VERBOSE("%s, Event : 0x%x", __FUNCTION__, event);
@@ -103,6 +121,14 @@ static void queue_int_handle_evt(UINT16 event, char *p_param)
 
         case BTIF_QUEUE_ADVANCE_EVT:
             queue_int_advance();
+            break;
+
+        case BTIF_QUEUE_CHECK_CONNECT_REQ:
+            queue_check_connect((connect_node_t*)p_param);
+            break;
+
+        default:
+            BTIF_TRACE_VERBOSE("Unknown Event");
             break;
     }
 
@@ -157,4 +183,23 @@ void btif_queue_advance() {
 void btif_queue_release() {
     list_free(connect_queue);
     connect_queue = NULL;
+}
+/*******************************************************************************
+**
+** Function         btif_queue_remove_connect
+**
+** Description      Remove connection request from connect Queue
+**                  when connect request for same UUID is received
+**                  from app.
+**
+** Returns          void
+**
+*******************************************************************************/
+void btif_queue_remove_connect(uint16_t uuid, uint8_t check_connect_req)
+{
+    connect_node_t node;
+    memset(&node, 0, sizeof(connect_node_t));
+    node.uuid = uuid;
+    btif_transfer_context(queue_int_handle_evt, check_connect_req,
+                      (char*)&node, sizeof(connect_node_t), NULL);
 }