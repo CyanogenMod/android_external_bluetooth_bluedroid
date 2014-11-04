codeaurora-unoffical/platform-external-bluetooth-bluedroid

Bluetooth: Fix to avoid buffer corruption while rebuilding the Bnep header
A case, where 3 remote device(PANU role) is connected with DUT(NAP role).
the one remote(panu role) send BNEP_COMPRESSED_ETHERNET_DEST_ONLY
packet to DUT(NAP role), which is for another remote device(panu role).
So DUT(NAP role) forward this packet to another remote device(panu role).
As same pbuf is used across the L2CAP/BNEP/PAN layer since reception from
L2CAP to writing on L2CAP CID of another PANU. Sometimes buffer(pbuf) gets
corrupted, when required buffer size is greater than actual buffer size,
while rebuilding the Bnep header. So using bnep_write instead of
bnep_writeBuf to solve the buffer corruption issue, which allocates
the new buffer & append data into it.

Change-Id: I125cab6d3da0f8126605bc7e9f764a27ee72fb1f
CRs-Fixed: 695286

stack/pan/pan_main.c
@@ -608,10 +608,13 @@ void pan_data_buf_ind_cb (UINT16 handle,
         dst_pcb = pan_get_pcb_by_addr (dst);
         if (dst_pcb)
         {
-            PAN_TRACE_EVENT0 ("pan_data_buf_ind_cb - destination PANU found and sending the data");
-            result = BNEP_WriteBuf (dst_pcb->handle, dst, p_buf, protocol, src, ext);
+            PAN_TRACE_WARNING3 ("%s - destination PANU found on handle %d and sending data, len: %d",
+                __FUNCTION__, dst_pcb->handle, len);
+
+            result = BNEP_Write (dst_pcb->handle, dst, p_data, len, protocol, src, ext);
             if (result != BNEP_SUCCESS && result != BNEP_IGNORE_CMD)
                 PAN_TRACE_ERROR1 ("Failed to write data for PAN connection handle %d", dst_pcb->handle);
+            GKI_freebuf (p_buf);
             return;
         }
     }

CyanogenMod/android_external_bluetooth_bluedroid

stack/pan/pan_main.c
@@ -610,10 +610,13 @@ void pan_data_buf_ind_cb (UINT16 handle,
         dst_pcb = pan_get_pcb_by_addr (dst);
         if (dst_pcb)
         {
-            PAN_TRACE_EVENT ("pan_data_buf_ind_cb - destination PANU found and sending the data");
-            result = BNEP_WriteBuf (dst_pcb->handle, dst, p_buf, protocol, src, ext);
+            PAN_TRACE_WARNING ("%s - destination PANU found on handle %d and sending data, len: %d",
+                __FUNCTION__, dst_pcb->handle, len);
+
+            result = BNEP_Write (dst_pcb->handle, dst, p_data, len, protocol, src, ext);
             if (result != BNEP_SUCCESS && result != BNEP_IGNORE_CMD)
                 PAN_TRACE_ERROR ("Failed to write data for PAN connection handle %d", dst_pcb->handle);
+            GKI_freebuf (p_buf);
             return;
         }
     }