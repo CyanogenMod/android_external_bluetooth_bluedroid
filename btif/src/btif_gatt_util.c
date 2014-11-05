b63d9422956a5c7d91ae44bbfc12a4875154d63b
Nitin Arora <niarora@codeaurora.org>
2014-11-04 18:36:11 -0800

    Bluetooth: Unpair LE device in case of Pin missing
    This change forces the host to remove the LE keys in case
    the remote responds to encryption with Pin or key missing.
    
    CRs-Fixed: 751286
    Change-Id: I2ef35fab7dc028ad29aaf6eeba1235478becb749

--- btif/src/btif_gatt_util.c
+++ btif/src/btif_gatt_util.c
@@ -48,6 +48,8 @@ static unsigned char BASE_UUID[16] = {
     0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
+extern bt_status_t btif_dm_remove_bond(const bt_bdaddr_t *bd_addr);
+
 int uuidType(unsigned char* p_uuid)
 {
     int i = 0;
@@ -298,11 +300,12 @@ static void btif_gatt_set_encryption_cb (BD_ADDR bd_addr, tBTA_TRANSPORT transpo
     UNUSED(bd_addr);
     UNUSED(transport);
 
-    if (result != BTA_SUCCESS && result != BTA_BUSY && result != BTA_HOST_DISCONN &&
-        result != BTA_FAILED_ESTABLISH && result != BTA_TIMEOUT && result != BTA_WRONG_MODE &&
-        result != BTA_PEER_DISCONN && result != BTA_LMP_TIMEOUT)
+    BTIF_TRACE_WARNING("%s() - Encryption failed (%d)", __FUNCTION__, result);
+    if(result == BTA_ERR_KEY_MISSING)
     {
-        BTIF_TRACE_WARNING("%s() - Encryption failed (%d)", __FUNCTION__, result);
+        bt_bdaddr_t bda;
+        bdcpy(bda.address, bd_addr);
+        btif_dm_remove_bond(&bda);
     }
 }
 
