codeaurora-unoffical/platform-external-bluetooth-bluedroid

Bluetooth: GAP: Disallow roleswitch for blacklisted devices
Set link policy to disallow role switch for blacklisted devices
as some devices not starting encryption after role switch
and a2dp connection getting delayed for some devices due to role switch

Change-Id: I07737f9ce72d68e24e4d8c460557c1ee2e8a64da
CRs-fixed: 733035

stack/btm/btm_acl.c
@@ -47,6 +47,37 @@ static void btm_process_remote_ext_features (tACL_CONN *p_acl_cb, UINT8 num_read
 
 #define BTM_DEV_REPLY_TIMEOUT   3       /* 3 second timeout waiting for responses */
 
+/* Black listed car kits/headsets for role switch */
+static const UINT8 btm_role_switch_black_list_prefix[][3] = {{0x00, 0x0d, 0xfd}, /* MOT EQ5 */
+                                                             {0x00, 0x1b, 0xdc}  /* BSHSBE20 */
+                                                            };
+
+/*******************************************************************************
+**
+** Function         btm_blacklistted_for_role_switch
+**
+** Description      This function is called to find the blacklisted carkits
+**                  for role switch.
+**
+** Returns          TRUE, if black listed
+**
+*******************************************************************************/
+BOOLEAN btm_blacklistted_for_role_switch (BD_ADDR addr)
+{
+    int blacklistsize = 0;
+    int i =0;
+
+    blacklistsize = sizeof(btm_role_switch_black_list_prefix)/sizeof(btm_role_switch_black_list_prefix[0]);
+    for (i=0; i < blacklistsize; i++)
+    {
+        if (0 == memcmp(btm_role_switch_black_list_prefix[i], addr, 3))
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
 /*******************************************************************************
 **
 ** Function         btm_save_remote_device_role
@@ -1072,6 +1103,11 @@ tBTM_STATUS BTM_SetLinkPolicy (BD_ADDR remote_bda, UINT16 *settings)
     if (*settings != HCI_DISABLE_ALL_LM_MODES)
     {
         if ( (*settings & HCI_ENABLE_MASTER_SLAVE_SWITCH) && (!HCI_SWITCH_SUPPORTED(localFeatures)) )
+        {
+            *settings &= (~HCI_ENABLE_MASTER_SLAVE_SWITCH);
+            BTM_TRACE_API1 ("BTM_SetLinkPolicy switch not supported (settings: 0x%04x)", *settings );
+        }
+        if ( (*settings & HCI_ENABLE_MASTER_SLAVE_SWITCH) && (btm_blacklistted_for_role_switch(remote_bda)) )
         {
             *settings &= (~HCI_ENABLE_MASTER_SLAVE_SWITCH);
             BTM_TRACE_API1 ("BTM_SetLinkPolicy switch not supported (settings: 0x%04x)", *settings );

CyanogenMod/android_external_bluetooth_bluedroid

stack/btm/btm_acl.c
@@ -48,6 +48,37 @@ static void btm_process_remote_ext_features (tACL_CONN *p_acl_cb, UINT8 num_read
 
 #define BTM_DEV_REPLY_TIMEOUT   3       /* 3 second timeout waiting for responses */
 
+/* Black listed car kits/headsets for role switch */
+static const UINT8 btm_role_switch_black_list_prefix[][3] = {{0x00, 0x0d, 0xfd}, /* MOT EQ5 */
+                                                             {0x00, 0x1b, 0xdc}  /* BSHSBE20 */
+                                                            };
+
+/*******************************************************************************
+**
+** Function         btm_blacklistted_for_role_switch
+**
+** Description      This function is called to find the blacklisted carkits
+**                  for role switch.
+**
+** Returns          TRUE, if black listed
+**
+*******************************************************************************/
+BOOLEAN btm_blacklistted_for_role_switch (BD_ADDR addr)
+{
+    int blacklistsize = 0;
+    int i =0;
+
+    blacklistsize = sizeof(btm_role_switch_black_list_prefix)/sizeof(btm_role_switch_black_list_prefix[0]);
+    for (i=0; i < blacklistsize; i++)
+    {
+        if (0 == memcmp(btm_role_switch_black_list_prefix[i], addr, 3))
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
 /*******************************************************************************
 **
 ** Function         btm_acl_init
@@ -1097,6 +1128,11 @@ tBTM_STATUS BTM_SetLinkPolicy (BD_ADDR remote_bda, UINT16 *settings)
     if (*settings != HCI_DISABLE_ALL_LM_MODES)
     {
         if ( (*settings & HCI_ENABLE_MASTER_SLAVE_SWITCH) && (!HCI_SWITCH_SUPPORTED(localFeatures)) )
+        {
+            *settings &= (~HCI_ENABLE_MASTER_SLAVE_SWITCH);
+            BTM_TRACE_API ("BTM_SetLinkPolicy switch not supported (settings: 0x%04x)", *settings );
+        }
+        if ( (*settings & HCI_ENABLE_MASTER_SLAVE_SWITCH) && (btm_blacklistted_for_role_switch(remote_bda)) )
         {
             *settings &= (~HCI_ENABLE_MASTER_SLAVE_SWITCH);
             BTM_TRACE_API ("BTM_SetLinkPolicy switch not supported (settings: 0x%04x)", *settings );