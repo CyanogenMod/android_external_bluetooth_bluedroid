f32e5742b8a6f4af9ba7df37d434d4e6841030fb
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-31 21:13:05 -0600

    a2dp: kill more logspam
    * srsly...why?
    
    Change-Id: I68d8aa78e2be66a2e9c74801f058c2ec04a53c75

--- audio_a2dp_hw/audio_a2dp_hw.c
+++ audio_a2dp_hw/audio_a2dp_hw.c
@@ -798,7 +798,7 @@ static size_t out_get_buffer_size(const struct audio_stream *stream)
 {
     struct a2dp_stream_out *out = (struct a2dp_stream_out *)stream;
 
-    INFO("buffer_size : %zu", out->common.buffer_sz);
+    DEBUG("buffer_size : %zu", out->common.buffer_sz);
 
     return out->common.buffer_sz;
 }
