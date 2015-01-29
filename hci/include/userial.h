CyanogenMod/android_external_bluetooth_bluedroid

commit f9f2dbcca4263188df5efe28b1a5bcb0ce906b6f

Bluedroid: Add support to send h/w error event.
Bluedroid stack needs to be reset whenever H/w chip restarts
like Subsytem restart.

CRs-fixed: 514760
Change-Id: I5888f0c7a29eb540b77c789774d672180f330168

hci/include/userial.h
@@ -65,7 +65,6 @@ void userial_close_reader(void);
 // less than |len|. This function will not block.
 uint16_t userial_read(uint16_t msg_id, uint8_t *p_buffer, uint16_t len);
 
-// Writes a maximum of |len| bytes from |p_data| to the serial port.
-// This function returns the number of bytes actually written, which may be
-// less than |len|. This function may block.
-uint16_t userial_write(uint16_t msg_id, const uint8_t *p_data, uint16_t len);
+#ifdef QCOM_WCN_SSR
+uint8_t userial_dev_inreset();
+#endif

hci/Android.mk
@@ -18,6 +18,12 @@ ifeq ($(BLUETOOTH_HCI_USE_MCT),true)
 
 LOCAL_CFLAGS += -DHCI_USE_MCT
 
+ifeq ($(QCOM_BT_USE_SMD_TTY),true)
+
+LOCAL_CFLAGS += -DQCOM_WCN_SSR
+
+endif
+
 LOCAL_SRC_FILES += \
 	src/hci_mct.c \
 	src/userial_mct.c

hci/src/hci_mct.c
@@ -739,6 +739,10 @@ uint16_t hci_mct_receive_evt_msg(void)
     uint8_t     msg_received;
     tHCI_RCV_CB  *p_cb=&mct_cb.rcv_evt;
     uint8_t     continue_fetch_looping = TRUE;
+#ifdef QCOM_WCN_SSR
+    uint8_t     dev_ssr_event[3] = { 0x10, 0x01, 0x0A };
+    uint8_t     reset;
+#endif
 
     while (continue_fetch_looping)
     {
@@ -747,15 +751,39 @@ uint16_t hci_mct_receive_evt_msg(void)
         {
             break;
         }
-
-        bytes_read++;
-        msg_received = FALSE;
-
-        switch (p_cb->rcv_state)
+#ifdef QCOM_WCN_SSR
+        reset = userial_dev_inreset();
+        if (reset)
         {
-        case MCT_RX_NEWMSG_ST:
-            /* Start of new message */
-            /* Initialize rx parameters */
+           /* Allocate a buffer for sending H/w error message */
+           if (bt_hc_cbacks)
+           {
+                len = 3 + BT_HC_HDR_SIZE;
+                p_cb->p_rcv_msg = \
+                  (HC_BT_HDR *) bt_hc_cbacks->alloc(len);
+           }
+           ALOGE("sending H/w error event to stack\n ");
+           p_cb->p_rcv_msg->offset = 0;
+           p_cb->p_rcv_msg->layer_specific = 0;
+           p_cb->p_rcv_msg->event = MSG_HC_TO_STACK_HCI_EVT;
+           p_cb->p_rcv_msg->len = 3;
+           memcpy((uint8_t *)(p_cb->p_rcv_msg + 1),&dev_ssr_event, 3);
+           msg_received = TRUE;
+           /* Next, wait for next message */
+           p_cb->rcv_state = MCT_RX_NEWMSG_ST;
+           continue_fetch_looping = FALSE;
+        }
+        else
+#endif
+        {
+          bytes_read++;
+          msg_received = FALSE;
+
+          switch (p_cb->rcv_state)
+          {
+            case MCT_RX_NEWMSG_ST:
+             /* Start of new message */
+               /* Initialize rx parameters */
             memset(p_cb->preload_buffer, 0 , 6);
             p_cb->preload_buffer[0] = byte;
             p_cb->preload_count = 1;
@@ -877,6 +905,7 @@ uint16_t hci_mct_receive_evt_msg(void)
             break;
         }
 
+       }
 
         /* If we received entire message, then send it to the task */
         if (msg_received)

hci/src/userial_mct.c
@@ -32,6 +32,10 @@
 #include <errno.h>
 #include <stdio.h>
 #include <sys/socket.h>
+#ifdef QCOM_WCN_SSR
+#include <termios.h>
+#include <sys/ioctl.h>
+#endif
 #include "bt_hci_bdroid.h"
 #include "userial.h"
 #include "utils.h"
@@ -207,6 +211,39 @@ static void *userial_read_thread(void *arg)
 
     return NULL;    // Compiler friendly
 }
+#ifdef QCOM_WCN_SSR
+/*******************************************************************************
+**
+** Function        userial_dev_inreset
+**
+** Description     checks for H/w reset events
+**
+** Returns         reset status
+**
+*******************************************************************************/
+
+uint8_t userial_dev_inreset()
+{
+    volatile int serial_bits;
+    uint8_t dev_reset_done =0, retry_count = 0;
+     ioctl(userial_cb.fd[CH_EVT], TIOCMGET, &serial_bits);
+     if (serial_bits & TIOCM_OUT2) {
+        while(serial_bits & TIOCM_OUT1) {
+             ALOGW("userial_device in reset \n");
+             utils_delay(2000);
+             retry_count++;
+             ioctl(userial_cb.fd[CH_EVT], TIOCMGET, &serial_bits);
+             if((serial_bits & TIOCM_OUT1))
+                dev_reset_done = 0;
+             else
+                dev_reset_done = 1;
+             if(retry_count == 6)
+               break;
+          }
+     }
+    return dev_reset_done;
+}
+#endif
 
 
 /*****************************************************************************

commit 5e78adca9cd8039d9c3ccee1ee730f71116c8657

bluedroid: Fix warning caused by commit f9f2dbcca4263188df5efe28b1a5bcb0ce906b6f
This commit (adding WCN_SSR) appears to have accidentally deleted
this function prototype when adding a new prototype.  Restore it
to fix a compilation warning.

Change-Id: I7a323af90dda49183bc49d2674e34237632f2b45

hci/include/userial.h
@@ -65,6 +65,11 @@ void userial_close_reader(void);
 // less than |len|. This function will not block.
 uint16_t userial_read(uint16_t msg_id, uint8_t *p_buffer, uint16_t len);
 
+// Writes a maximum of |len| bytes from |p_data| to the serial port.
+// This function returns the number of bytes actually written, which may be
+// less than |len|. This function may block.
+uint16_t userial_write(uint16_t msg_id, const uint8_t *p_data, uint16_t len);
+
 #ifdef QCOM_WCN_SSR
 uint8_t userial_dev_inreset();
 #endif