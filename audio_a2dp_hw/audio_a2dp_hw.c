b3fed713fa84e4e5bb71524df9567986c6533eb6
Christopher R. Palmer <crpalmer@gmail.com>
2015-02-13 08:51:10 -0500

    bluetooth: a2dp: Fix two race conditions in out_write
    In the case of a write error the datapath is disconnected.
    However, this disconnection is not protected by the mutex
    and it's possible for another thread to concurrently suspend
    or cause the stream to exit (via out_set_parameters).
    
    Simulating this race condition by adding a sleep between
    the skt_disconnect and clearing of the fd did cause the
    a2dp playback to fail until the next time that the stream
    was paused and resumed.
    
    The second race condition is, as far as I can tell, really
    theoretical.  After the unlock and before skt_write it is
    possible for the fd to be concurrently closed (as above)
    and then that same fd to be opened for some other use and
    consequently for us to write data to some other fd and
    cause all sorts of havoc.
    
    I was not able to reproduce any case of this occurring though.
    
    Change-Id: I770368697f6ad6bc4b023efd09f5d7a2b947b405

--- audio_a2dp_hw/audio_a2dp_hw.c
+++ audio_a2dp_hw/audio_a2dp_hw.c
@@ -738,7 +738,6 @@ static ssize_t out_write(struct audio_stream_out *stream, const void* buffer,
 #if 0
     ts_error_log("a2dp_out_write", bytes, out->common.buffer_sz, out->common.cfg);
 #endif
-    pthread_mutex_unlock(&out->common.lock);
 
     if (perf_systrace_log_enabled)
     {
@@ -763,6 +762,8 @@ static ssize_t out_write(struct audio_stream_out *stream, const void* buffer,
         else
             ERROR("write failed : stream suspended, avoid resetting state");
     }
+
+    pthread_mutex_unlock(&out->common.lock);
 
     DEBUG("wrote %d bytes out of %zu bytes", sent, bytes);
     return sent;
