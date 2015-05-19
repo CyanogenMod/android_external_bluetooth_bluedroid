58c949b5197be615b7b92ea9d80472fe7da7e6a3
Pat Erley <perley@cyngn.com>
2015-05-19 09:54:56 -0700

    Revert "bluedroid: Support dynamic A2DP stream parameters"
    This reverts commit f322d1c9eb96d3de4721439654c3bcd53d65280f.
    
    This resolves issues some users have seen with a2dp playback with
    certain car audio head units.
    
    BACON-3275
    Change-Id: I968d2652a71d2867bf29bf1a60e78fe692709c9f
    (cherry picked from commit 084209967ca9113ca04fe1e5daf26cf90fa7f0e0)

--- audio_a2dp_hw/audio_a2dp_hw.h
+++ audio_a2dp_hw/audio_a2dp_hw.h
@@ -36,7 +36,11 @@
 #define A2DP_CTRL_PATH "/data/misc/bluedroid/.a2dp_ctrl"
 #define A2DP_DATA_PATH "/data/misc/bluedroid/.a2dp_data"
 
+#ifdef SAMPLE_RATE_48K
+#define AUDIO_STREAM_DEFAULT_RATE          48000
+#else
 #define AUDIO_STREAM_DEFAULT_RATE          44100
+#endif
 #define AUDIO_STREAM_DEFAULT_FORMAT        AUDIO_FORMAT_PCM_16_BIT
 #define AUDIO_STREAM_DEFAULT_CHANNEL_FLAG  AUDIO_CHANNEL_OUT_STEREO
 #define AUDIO_STREAM_OUTPUT_BUFFER_SZ      (20*512)
@@ -50,7 +54,6 @@ typedef enum {
     A2DP_CTRL_CMD_STOP,
     A2DP_CTRL_CMD_SUSPEND,
     A2DP_CTRL_GET_AUDIO_CONFIG,
-    A2DP_CTRL_SET_AUDIO_CONFIG,
 } tA2DP_CTRL_CMD;
 
 typedef enum {
@@ -76,19 +79,6 @@ typedef enum {
 **  Functions
 ******************************************************************************/
 
-inline bool is_supported_sample_rate(uint32_t sample_rate) {
-    return sample_rate == 8000  || sample_rate == 12000 || sample_rate == 16000 ||
-           sample_rate == 24000 || sample_rate == 32000 || sample_rate == 48000 ||
-           sample_rate == 11025 || sample_rate == 22050 || sample_rate == 44100;
-}
-
-inline bool is_supported_channel_count(uint8_t channel_count) {
-    return channel_count == 1 || channel_count == 2;
-}
-
-inline bool is_supported_bit_depth(uint8_t bit_per_sample) {
-    return bit_per_sample == 16;
-}
 
 /*****************************************************************************
 **
