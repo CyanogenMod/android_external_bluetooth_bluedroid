b880c2c6dc7e5e08e5aa6b275b71767e44f73047
Ajay Panicker <apanicke@google.com>
2016-03-17 17:09:24 -0700

    Add guest mode functionality (2/3)
    Add a flag to enable() to start Bluetooth in restricted
    mode. In restricted mode, all devices that are paired during
    restricted mode are deleted upon leaving restricted mode.
    Right now restricted mode is only entered while a guest
    user is active.
    
    CYNGNOS-3020
    Bug: 27410683
    Change-Id: I8f23d28ef0aa3a8df13d469c73005c8e1b894d19
    (cherry picked from commit cfa2b3924a9f87939827185a4061c70515b5800c)

--- test/suite/cases/adapter.c
+++ test/suite/cases/adapter.c
@@ -24,7 +24,7 @@
 bool adapter_enable_disable() {
   int error;
 
-  CALL_AND_WAIT(error = bt_interface->enable(), adapter_state_changed);
+  CALL_AND_WAIT(error = bt_interface->enable(false), adapter_state_changed);
   TASSERT(error == BT_STATUS_SUCCESS, "Error enabling Bluetooth: %d", error);
   TASSERT(adapter_get_state() == BT_STATE_ON, "Adapter did not turn on.");
 
